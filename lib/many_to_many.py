3f02597d04f814fade5d29e634291a944339849e
CJ <cy.jerotich@gmail.com>
2024-05-25 23:58:45 +0300

    done

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,28 @@
 class Author:
-    pass
+    def __init__(self, name):
+        self.name = name
+
 
 
 class Book:
-    pass
+    def __init__(self, title):
+        self.title = title
+        self._contracts = []
+
 
 
 class Contract:
-    pass
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(author, Author):
+            raise TypeError("author must be an instance of Author")
+        if not isinstance(book, Book):
+            raise TypeError("book must be an instance of Book")
+        if not isinstance(date, str):
+            raise TypeError("date must be a string")
+        if not isinstance(royalties, int):
+            raise TypeError("royalties must be an integer")
+
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
